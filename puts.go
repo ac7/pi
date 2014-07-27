61f9487efcee35dbe8b2407b7edc92cafe7ab755
ac7 <ac7@users.noreply.github.com>
2014-07-27 14:55:19 -0500

    Fix cursor alignment

--- puts.go
+++ puts.go
@@ -5,11 +5,11 @@ import "github.com/nsf/termbox-go"
 func puts(x, y int, str string, fg, bg termbox.Attribute) {
 	pos := 0
 	for _, b := range str {
+		termbox.SetCell(x+pos, y, b, fg, bg)
 		if b == '\t' {
 			pos += 8
 		} else {
 			pos++
 		}
-		termbox.SetCell(x+pos, y, b, fg, bg)
 	}
 }

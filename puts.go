7a46deab8bf8db6ec3c540c8ef26142a7cca36f5
ac7 <ac7@users.noreply.github.com>
2014-07-27 14:53:46 -0500

    Display tabs as eight spaces

--- puts.go
+++ puts.go
@@ -3,7 +3,13 @@ package main
 import "github.com/nsf/termbox-go"
 
 func puts(x, y int, str string, fg, bg termbox.Attribute) {
-	for i, b := range str {
-		termbox.SetCell(x+i, y, b, fg, bg)
+	pos := 0
+	for _, b := range str {
+		if b == '\t' {
+			pos += 8
+		} else {
+			pos++
+		}
+		termbox.SetCell(x+pos, y, b, fg, bg)
 	}
 }
